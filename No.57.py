228683924d01b64a5a23b61db39350f48e345c84
yusuke <byysowbach21@gmail.com>
2024-12-03 00:28:45 +0900

    2024/12/3

---
+++ No.57.py
@@ -0,0 +1,16 @@
+# yukicoder_no.57
+
+N = int(input())
+print(N * 3.5)
+
+# 期待値の線形性
+# https://manabitimes.jp/math/698
+
+
+# 別解
+import statistics
+lst = range(N, N * 6 + 1)
+print(statistics.median(lst))
+
+# statistics は平均値, 中央値, 最頻値, 分散, 標準偏差などを求めるのに便利
+
